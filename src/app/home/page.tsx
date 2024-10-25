8216ccfef529f97cf5fc1b96fd9796d96150f002
vandoslando@gmail.com <vandoslando@gmail.com>
2024-10-25 21:22:18 +0300

    file v1.4

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,13 @@
 
 import HomeLayout from './home';
 
+interface PageProps {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+  children: React.ReactNode
+}
 
-export default function Home({children}: {children: React.ReactNode}) {
+export default function Home({children}: PageProps) {
   
   return (
     <>
